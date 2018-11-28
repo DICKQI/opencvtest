cd28d52ab401ee5070ac295d9eda450b65dce804
DICKQI <dickqi87@gmail.com>
2018-11-28 17:30:47 +0800

    change

--- test2.py
+++ test2.py
@@ -1,4 +1,26 @@
 import cv2 as cv
+import numpy as np
+def extrace_object_demo():
+    capture = cv.VideoCapture(0)
+    while True:
+        ret, frame = capture.read()
+        if ret == False:
+            break
+        '''过滤出红色部分'''
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        lower_hsv = np.array([156, 43, 46])
+        upper_hsv = np.array([180, 255, 255])
+        time1 = cv.getTickCount()
+        mask = cv.inRange(hsv, lowerb=lower_hsv, upperb=upper_hsv)
+        time2 = cv.getTickCount()
+        print((time2 - time1) / cv.getTickFrequency())
+        cv.imshow("video", frame) # 将帧显示出来
+        cv.imshow("mask", mask)
+        c = cv.waitKey(40)
+        if c == 27:
+            break
+
+
 def color_space_demo(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     cv.imshow("gray", gray)
@@ -17,7 +39,7 @@ try:
     src = cv.imread('red.jpg')
     cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
     cv.imshow("input image", src)
-    color_space_demo(src)
+    extrace_object_demo()
     cv.waitKey(0)
 except:
     pass
